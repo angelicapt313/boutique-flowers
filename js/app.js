03412a694cc51139e56bf935943123096b551608
Ag <puchetatome.angelica@gmail.com>
2024-03-25 14:57:29 -0600

    Added a function to add a card to the shopping cart.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const cardsFlowers = document.querySelector('.list_flowers');
+const tableCart = document.querySelector('#cart_list tbody');
 
-
+let arrCart = [];
 
 eventListeners();
 function eventListeners(){
-    cardsFlowers.addEventListener('click', addCart)
+    cardsFlowers.addEventListener('click', addCart);
 }
 
 // Función agregar carrito
@@ -12,7 +13,9 @@ function addCart(e){
     if(e.target.classList.contains('add')){
         const cardSelected = e.target.parentElement;
         readData(cardSelected);
+        cartHtml();
     }
+    
 }
 
 // Leer datos de la tarjeta
@@ -24,5 +27,25 @@ function readData(card){
         id: card.querySelector('.add').getAttribute('data-id'),
         quantity: 1
     }
+
+    arrCart = [...arrCart, infoCard];
+   
 }
 
+// Agregar información al carrito
+function cartHtml(){
+    arrCart.forEach(cr => {
+        const {img, price, title, id, quantity} = cr;
+        // Crear renglon
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td><img src="${img}" width="50px"> </td>
+        <td>${price}</td>
+        <td>${title}</td>
+        <td>${quantity}</td>
+        <td><a href="#" class="btn btn-danger" data-id="${id}">X</a></td>
+        `;
+
+        tableCart.appendChild(row);
+    });
+}
